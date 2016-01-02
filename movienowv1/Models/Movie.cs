06b239d4c25b3f6d762f0378dfaca253cd97a59d
Tatiana Manoli <github@atypicalrobot.com>
2016-01-02 19:09:30 +0000

    Add movie model.

--- movienowv1/Models/Movie.cs
+++ movienowv1/Models/Movie.cs
@@ -7,5 +7,15 @@ namespace movienowv1.Models
 {
     public class Movie
     {
-    }  
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string CoverURL { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public float ImdbRating { get; set; }
+        // running time is being stored in minutes
+        public int Runtime { get; set; }
+
+
+    }
 }
